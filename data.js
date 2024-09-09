7452541d2da78757af08ff5d0acc75cf686c557c
Ian Vincent L. Reyes <99298221+IanReyes2@users.noreply.github.com>
2024-09-10 00:19:53 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+ID: 1,
+Name: "Rx-0",
+Profession: "Sinanju Killer",
+},
+{
+ID: 2,
+Name: "Rx-78-2",
+Profession: "Dom Killer",
+},
+{
+ID: 3,
+Name: "Msz-006",
+Profession: "Quebeley hunter",
+},
+{
+ID: 4,
+Name: "Zgmf X10-A",
+Profession: "Freedom giver",
+},
+{
+ID: 5,
+Name: "XXG 00W0",
+Profession: "Wing gundam",
+},
+{
+ID: 6,
+Name: "Ms-06F",
+Profession: "The Red Comet",
+},
+];
